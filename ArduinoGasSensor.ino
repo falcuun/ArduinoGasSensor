d6e3de7e776167c490df5f02bac39a5c9c6e937e
falcuun <falcuun@gmail.com>
2019-04-04 22:53:19 +0200

    Including Libraries

---
+++ ArduinoGasSensor.ino
@@ -0,0 +1,11 @@
+#include <SPI.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup(){}
+void loop(){}
