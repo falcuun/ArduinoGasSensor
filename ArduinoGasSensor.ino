5992cd4c2c8057ed05f47574524041b5392058a4
falcuun <falcuun@gmail.com>
2019-04-05 00:00:17 +0200

    Added test for reading from sensor

--- ArduinoGasSensor.ino
+++ ArduinoGasSensor.ino
@@ -4,8 +4,24 @@
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define GAS_SENSOR_A0 A0
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-void setup(){}
-void loop(){}
+void setup()
+{
+    pinMode(GAS_SENSOR_A0, INPUT);
+    SPI.begin();
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+}
+
+void loop()
+{
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(27, 30);
+    display.print(analogRead(GAS_SENSOR_A0));
+    display.display();
+    delay(200);
+}
